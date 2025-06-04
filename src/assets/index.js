be778ebaa1ccfc841e2729a908244808f6e5f1d9
Brostouch KOK <brostouchkokit1007@gmail.com>
2025-06-04 19:41:26 +0700

    setup folder structure ans config tailwindcss

---
+++ src/assets/index.js
@@ -0,0 +1,22 @@
+import logo from "./images/logo.png";
+import bannerImg from "./images/bannerImg.png";
+import projectOne from "./images/projects/projectOne.jpg";
+import projectTwo from "./images/projects/projectTwo.jpg";
+import projectThree from "./images/projects/projectThree.jpeg";
+import testimonailOne from "./images/testmonial/testimonialOne.png";
+import testimonailTwo from "./images/testmonial/testimonialTwo.png";
+import quote from "./images/testmonial/quote.png";
+import contactImg from "./images/contact/contactImg.png";
+
+export{
+    logo,
+    bannerImg,
+    projectOne,
+    projectTwo,
+    projectThree,
+    testimonailOne,
+    testimonailTwo,
+    quote,
+    contactImg,
+}
+
