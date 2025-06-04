73bf94b10cee848c83ccd09863c2fb0f0a659785
Brostouch KOK <brostouchkokit1007@gmail.com>
2025-06-05 00:15:46 +0700

    completed features section

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import { AiFillAppstore } from "react-icons/ai";
+import { FaMobile, FaGlobe, FaBars } from "react-icons/fa";
+import { SiProgress, SiAntdesign } from "react-icons/si";
+import Title from "../layout/Title";
+import Card from "./Card";
 
 const Features = () => {
   return (
-    <section id='features' className='w-full h-[800px] pb-20 flex border-b-[1px] border-b-black'>
-      feature
+    <section
+      id="features"
+      className="w-full py-20 border-b-[1px] border-b-black"
+    >
+      <Title title={"Features"} desc={"What I Do"} />
+      <div className="grid grid-cols-3 gap-20">
+        <Card
+          title="Business Stratagy"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad"
+            icon={<FaBars/>}
+        />
+        <Card
+          title="App Development"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad"
+            icon={< AiFillAppstore/>}
+        />
+        <Card
+          title="SEO Optimisation"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad"
+        icon={<SiProgress/>}
+        />
+        <Card
+          title="Mobile Development"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad"
+        icon={<FaMobile/>}
+        />
+        <Card
+          title="UX Design"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad"
+        icon={<SiAntdesign/>}
+        />
+        <Card
+          title="Hosting Websites"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad"
+        icon={<FaGlobe/>}
+        />
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Features
+export default Features;
