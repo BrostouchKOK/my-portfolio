418b57e8242dce77c5c436e211210d9917007263
Brostouch KOK <brostouchkokit1007@gmail.com>
2025-06-05 21:51:35 +0700

    completed projects section

---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Title from "../layout/Title";
+import ProjectCard from "./ProjectCard";
+import { projectOne, projectTwo, projectThree } from "../../assets/index";
+
+const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="w-full py-20 border-b-[1px] border-b-black"
+    >
+      <div className="flex items-center justify-center text-center">
+        <Title
+          title="VISTI MY PORTFOLIO AND KEEP YOUR FEEDBACK"
+          desc="My Project"
+        />
+      </div>
+      <div className="grid grid-cols-3 gap-10">
+        <ProjectCard
+          src={projectOne}
+          title={"SOCIAL MEDIA CLONE"}
+          des={
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          }
+        />
+        <ProjectCard
+          src={projectTwo}
+          title={"E-commerce Website"}
+          des={
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          }
+        />
+        <ProjectCard
+          src={projectThree}
+          title={"Chatting App"}
+          des={
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          }
+        />
+        <ProjectCard
+          src={projectOne}
+          title={"SOCIAL MEDIA CLONE"}
+          des={
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          }
+        />
+        <ProjectCard
+          src={projectTwo}
+          title={"SOCIAL MEDIA CLONE"}
+          des={
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          }
+        />
+        <ProjectCard
+          src={projectThree}
+          title={"Chatting App"}
+          des={
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          }
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
